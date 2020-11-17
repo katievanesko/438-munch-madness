6d4c4212dbf6cc4e94b1099295c45fe14c99cb24
katie vanesko <kaitlinvanesko@wustl.edu>
2020-11-16 23:19:35 -0600

    very basic VCs

---
+++ munch-madness/munch-madness/PreferencesViewController.swift
@@ -0,0 +1,61 @@
+//
+//  PreferencesViewController.swift
+//  munch-madness
+//
+//  Created by Katie Vanesko on 11/16/20.
+//  Copyright © 2020 Katie Vanesko. All rights reserved.
+//
+
+import UIKit
+
+class PreferencesViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+
+    @IBOutlet weak var cuisinePicker: UIPickerView!
+    @IBAction func pricePicker(_ sender: Any) {
+    }
+    @IBOutlet weak var radiusPicker: UIPickerView!
+    @IBOutlet weak var findRestaurantsBtn: UIButton!
+    @IBOutlet weak var location: UITextView!
+    
+    
+    var cuisineData:[String] = []
+    var priceData:[String] = []
+    var radiusData:[String] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cuisineData = ["Any","American","Breakfast","Burgers", "Chinese","Italien", "Japanese","Korean" ,"Mexican","Pizza","Sandwhiches","Seafood"]
+        radiusData = ["Any", "1 mile","5 miles","10 miles","15 miles"]
+
+        cuisinePicker.dataSource = self
+        cuisinePicker.delegate = self
+  
+        radiusPicker.dataSource = self
+        radiusPicker.delegate = self
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0 {
+            return cuisineData.count
+        } else {
+            return radiusData.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 0 {
+            return cuisineData[row]
+        } else {
+            return radiusData[row]
+        }
+    }
+
+}
---
+++ munch-madness/munch-madness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  munch-madness
+//
+//  Created by Katie Vanesko on 11/16/20.
+//  Copyright © 2020 Katie Vanesko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
