defd91cb7343b9deb376f41bb4a4c6e5ed3b1f58
Michael Casey <casey.m@wustl.edu>
2020-11-25 12:10:23 -0600

    still working on DB stuff and read/write data

--- munch-madness/munch-madness/ViewController.swift
+++ munch-madness/munch-madness/ViewController.swift
@@ -21,12 +21,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         ref = Database.database().reference()
         watchGroupData()
+        addUser()
 //        addGroup()
 //        addGroup()
 
     }
-//    from https://firebase.google.com/docs/database/ios/read-and-write
-    // from Lecture 9 video 32:41
+//      commented part from https://firebase.google.com/docs/database/ios/read-and-write
+    // other part from Lecture 9 video 32:41
     func watchGroupData(){
 //        var refHandle = ref.observe(DataEventType.value, with: { (snapshot) in
 //          let postDict = snapshot.value as? [String : AnyObject] ?? [:]
@@ -46,7 +47,15 @@ class ViewController: UIViewController {
         
     }
     
+    func addUser(){
+        // testing how to add users to an array in the database
+       // basically creating a set of users for each group - not quite an array, but similar vibe https://stackoverflow.com/questions/39815117/add-an-item-to-a-list-in-firebase-database
+        self.ref.child("groups").child("123456").child("users").child("putUserNameHere").setValue(true)
+
+    }
+    
     // based on solution 1 from https://izziswift.com/short-random-unique-alphanumeric-keys-similar-to-youtube-ids-in-swift/
+    // generates a random alphanumeric Pin for the game of length len
     func generatePin(len: Int) -> NSString{
     
         let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
