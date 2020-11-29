23980b381f1406dd6cb62b15594a6df8217b7a40
anda-gavrilescu <65573181+anda-gavrilescu@users.noreply.github.com>
2020-11-28 16:34:57 -0800

    fixed adduser problem

--- munch-madness/munch-madness/WaitingViewController.swift
+++ munch-madness/munch-madness/WaitingViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+import CoreData
 
-class WaitingViewController: UIViewController {
+class WaitingViewController: UIViewController, UITextFieldDelegate {
 
-    var name = "Guest"
+    var name: String?
+    var ref: DatabaseReference!
     
     @IBOutlet weak var nameField: UITextField!
-    
     
     @IBAction func nameFieldChanged(_ sender: Any) {
         if let newName = nameField.text {
@@ -21,9 +25,75 @@ class WaitingViewController: UIViewController {
         }
     }
     
+    @IBAction func enterPressed(_ sender: Any) {
+        addNewName()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        nameField.delegate = self
+    }
+    
+    func getNameAndCode() -> Array<String> {
+        var answers:Array<String> = []
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return answers
+        }
+
+        let managedContent = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "User")
+
+        do {
+            let results = try managedContent.fetch(fetchRequest)
+            answers.append(results[0].value(forKey: "name") as! String)
+            answers.append(results[0].value(forKey: "code") as! String)
+            return answers
+        } catch {
+            print("Could not fetch")
+            return answers
+        }
+    }
+    
+    //this all works
+    func addNewName() {
+        let results = getNameAndCode()
+        let oldName = results[0]
+        let code = results[1]
+        print("old name \(oldName)")
+        
+        if results.count == 2 {
+            if let newName = self.name {
+//                DispatchQueue.global(qos: .userInitiated).async {
+                    self.ref.child("groups").child(code).child("users").child(oldName).removeValue(completionBlock: { (error, ref) in
+                        print(error ?? "success")
+                    })
+                    
+                    self.ref.child("groups").child(code).child("users").child(newName).setValue(true)
+                
+//                    DispatchQueue.main.async {
+                        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                            return
+                        }
+
+                        let managedContent = appDelegate.persistentContainer.viewContext
+
+                        if let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContent) {
+                            let currentUser = NSManagedObject(entity: entity, insertInto: managedContent)
+                            currentUser.setValue(newName, forKey: "name")
+
+                            do {
+                                try managedContent.save()
+                            } catch {
+                                print("Could not save")
+                            }
+                        }
+//                    }
+//                }
+            }
+        }
     }
     
     /*
